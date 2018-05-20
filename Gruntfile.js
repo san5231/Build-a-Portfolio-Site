413fbb5d37f898fb5e7cac79ba34cfbebc3cd79c
san5231 <scchen17@gmail.com>
2018-05-19 22:04:39 -0700

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) { 
+	
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				src: 'src/sass/style.scss',
+				dest: 'dist/css/style.css'
+			}
+		},
+		concat: {
+			dist: {
+				src: 'src/js/*.js',
+				dest: 'dist/js/app.js'
+			}
+		}
+	});
+
+	grunt.registerTask('default', [
+		'sass',
+		'concat'
+	]);
+};
